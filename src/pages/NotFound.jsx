579a604b0cc31418638cb62b1bc972143a345cc0
Sena Oz <sena@maestra.ai>
2024-07-28 16:20:10 +0300

    update NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,15 +2,16 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="container text-center">
-      <div className="row">
-        <div className="col">
-          <h1 className="display-1">404</h1>
-          <p className="lead">Page Not Found</p>
-          <a href="/" className="btn btn-primary">
-            Go Home
-          </a>
-        </div>
+    <div
+      className="container text-center vh-100 d-flex justify-content-center align-items-center
+    "
+    >
+      <div>
+        <h1 className="display-1">404</h1>
+        <p className="lead">Page Not Found</p>
+        <a href="/" className="btn btn-primary">
+          Go Home
+        </a>
       </div>
     </div>
   );
